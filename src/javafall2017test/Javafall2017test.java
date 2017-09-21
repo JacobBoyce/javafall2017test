d99bc20299e172015bf1cda6465b51610f91860c
Jacob Boyce <boysee4545@gmail.com>
2017-09-21 11:40:57 -0600

    goodbye stuff

--- src/javafall2017test/Javafall2017test.java
+++ src/javafall2017test/Javafall2017test.java
@@ -17,7 +17,7 @@ public class Javafall2017test
      */
     public static void main(String[] args) 
     {
-        // TODO code application logic here
+        System.out.println("buhbye");
     }
     
 }
